2db6ffc62ab0d41c0407b8dc03a34efe0803c00a
Matthew Stuhr <36904235+MatthewStuhr@users.noreply.github.com>
2019-12-19 15:26:34 -0800

    Adding the app.js code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+let game = null;
+
+//adding event listener to the 'Start Game' Button 
+$('#btn__reset').on('click', function() {
+    game = new Game();
+    game.startGame();
+});
+
+//event listeners to each of the on screen keyboard buttons
+$('.key').on('click', function(e) {
+    game.handleInteraction(e.target);
+});
+
+//event listeners for physical keyboard, used to guess letters
+$(window).keydown(function(e) {
+    let clickedButton = null;
+    $('.key').each(function() {
+            if ($(this).text() === e.key) {
+                clickedButton = $(this)[0];
+            }
+        })
+    if (e.which > 64 && e.which < 91) {
+        game.handleInteraction(clickedButton);
+    }
+});
